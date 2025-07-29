0a79f58af9a82cf338f1326d030732d75431ba37
Yanwen <66867243+vin199306@users.noreply.github.com>
2025-07-29 17:58:46 +0800

    ffff

--- internal/cmd/get.go
+++ internal/cmd/get.go
@@ -12,7 +12,7 @@ var getCmd = &cobra.Command{
     Use:    "get {云盘路径}",
     PreRun: session.Parse,
     Short:  "获取云盘文件的详细信息",
-    Args:   cobra.ExactArgs(1),
+    Args:   ccobra.MinimumNArgs(2),
     Run: func(cmd *cobra.Command, args []string) {
         err := file.CheckPath(args...)
         if err != nil {
@@ -20,16 +20,35 @@ var getCmd = &cobra.Command{
             return
         }
         name := args[0]
+		
 		info, err := App().Stat(name)
 		if err != nil {
 			return
 		}
-		// 使用反射打印所有方法
-        fmt.Println("=== 文件对象支持的方法 ===")
-        t := reflect.TypeOf(info)
-        for i := 0; i < t.NumMethod(); i++ {
-            method := t.Method(i)
-            fmt.Printf("%s: %s\n", method.Name, method.Type)
+		// 使用反射调用指定方法
+		methodName := strings.Title(args[1])
+        value := reflect.ValueOf(info)
+        method := value.MethodByName(methodName)
+        
+        if !method.IsValid() {
+            fmt.Printf("错误：方法 %s 不存在\n", methodName)
+            fmt.Println("可用方法：Id, Name, Size, ModTime, IsDir, PId")
+            return
+        }
+        
+        // 调用方法
+        results := method.Call(nil)
+        
+        // 处理返回值
+        if len(results) > 0 {
+            // 格式化输出不同类型的返回值
+            for _, result := range results {
+                if result.IsValid() {
+                    fmt.Printf("%s: %v\n", methodName, result.Interface())
+                }
+            }
+        } else {
+            fmt.Printf("方法 %s 没有返回值\n", methodName)
         }
     },
 }
